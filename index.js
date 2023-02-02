ac8cfc05e7aa58b6bb0e530e6e4ef33dc2f6f2a8
russellgillespie <60009650+russellgillespie@users.noreply.github.com>
2023-02-02 02:01:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+'use strict';
+
+const init = () => {
+  
+  const html = document.getElementsByTagName('html').item(0),
+    game = document.getElementById('game');
     game.innerHTML = ""
     // canvas = document.getElementsByTagName('canvas').item(0),
     // context = canvas.getContext('2d'),
@@ -54,3 +60,145 @@
     vw < vh ? game.style.width = vw + "px": game.style.width = vh + "px";
     console.log(game.style.width);
     game.style.height = game.style.width;
+    vw < vh ? sliceSize = vw / (slices + 2) : sliceSize = vh / (slices + 2);
+    console.log(sliceSize);
+
+    // Create Interface buttons
+    let keyScore = addTileButton("Score: " + score, 27, sliceSize-2, 4*sliceSize+1, 1, colorUI, colorHover, null, null);
+    keyScore.style.width = ((sliceSize-1) * 2)+"px";
+    keyScore.removeEventListener ("mouseup", function() {
+    target.innerHTML += button.innerHTML;
+  });
+    keyScore.style.fontSize = 'small';
+    
+    let answer = addTileButton("",26, sliceSize * 3 - 2, 2*sliceSize+1, (sliceSize * 6)+1, colorUI, colorHover, null, {});
+    answer.style.height = sliceSize-2 +"px";
+    answer.style.fontSize = 'small'; 
+    
+     let enter = addTileButton("Enter",26, sliceSize - 1, (sliceSize*5)+1, (sliceSize * 6)+1, colorUI, colorHover, null, function(){
+      // Check if innerHTML is valid word
+       if (answer.innerHTML.toString().includes(abc[0])){
+         if (dictionary.search(answer.innerHTML.toLowerCase())) { 
+           score = updateScore(score, answer.innerHTML.length, keyScore);
+         } else { alert("Word not in list!")}
+       } else { alert("Key Letter not used!")}
+     });
+     enter.style.height = sliceSize-2 +"px";
+     enter.addEventListener ("mouseup", function(){
+       answer.innerHTML = "";
+     });
+     enter.style.fontSize = 'small'; 
+
+
+    let keyLetter = addTileButton(abc[0],25, sliceSize-2, (3*sliceSize)+1, 1, colorSecondary, colorHover, answer, function()
+            {
+              answer.innerHTML += abc[0];
+    });
+    
+    
+    let keyReset = addTileButton("Reset",25, 2*sliceSize-2, (sliceSize)+1, 1, colorUI, colorHover, answer, init);
+
+    //keyReset.addEventListener ("mouseup", init);
+    keyReset.style.fontSize = 'small';
+    keyReset.style.height = sliceSize-2 + "px";
+    
+    let keyDelete = addTileButton("Delete",28, sliceSize-2, sliceSize+1, (sliceSize * 6)+1, colorUI, colorHover, null, null);
+    keyDelete.removeEventListener ("mouseup", function() {
+    target.innerHTML += button.innerHTML;
+  });
+    keyDelete.addEventListener ("mouseup", function(){
+      answer.innerHTML = answer.innerHTML.substring(0,answer.innerHTML.length-1);
+    });
+    keyDelete.style.fontSize = 'small';
+    
+    let grid = [];
+    for (let col=1;col<slices+1;col++){
+        for (let row=1;row<slices+1;row++){
+          // Cache button metadata
+          let buttonIndex = (col-1)*slices+row-1;
+          let buttonText = randABC[buttonIndex];
+          let xpos = col*sliceSize;
+          let ypos = row*sliceSize;
+          let xoff = (col+1)*sliceSize;
+          let yoff = (row+1)*sliceSize;
+
+          let b = addTileButton(buttonText, buttonIndex, sliceSize-2, xpos+1, ypos+1, colorLight, colorHover, null, function()
+            {
+              answer.innerHTML += buttonText;
+            }
+          );
+          
+          if (col > 1 && col < 6 && row > 1 && row < 6){
+            addTileButton("","Slicer_"+row+"_"+col, sliceSize/10, xpos-sliceSize/20, ypos-sliceSize/20, colorUI, colorHover, null, null);
+          }
+          
+        }
+    }
+
+  };
+  
+  const loop = (t) => {
+    
+    i ++;
+    last = t;
+    window.requestAnimationFrame(loop);
+  };
+
+  let w,  h, last,
+    i = 0,
+    start = 0;
+
+  window.removeEventListener('load', init);
+  window.addEventListener('resize', resize);
+  resize();
+  drawMap();
+  html.classList.remove('no-js');
+  html.classList.add('js');
+  window.requestAnimationFrame(loop);
+};
+
+window.addEventListener('load', init);
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function addTileButton(label, index, size, x, y, color, hover, target, listener) {
+  // 1. Create the button
+  var game = document.getElementById("game");
+  var button = document.createElement("button");
+  button.name = index;
+  button.innerHTML = label;
+  button.setAttribute("class", "gameButton");
+  button.style.height = size+"px";
+  button.style.width = size+"px";
+  button.style.left = x+"px";
+  button.style.top = y+"px";
+  button.style.backgroundColor = color;
+  button.style.fontSize = 'x-large';
+  
+  // 2. Append somewhere
+  //var body = document.getElementsByTagName("body")[0];
+  game.appendChild(button);
+
+  // 3. Add event handler
+  button.addEventListener ("mouseup", listener);
+  
+  // highlight the mouseover target
+  button.addEventListener("mouseenter", (event) => {
+  event.target.style.backgroundColor = hover;
+  }, false);
+
+  button.addEventListener("mouseout", (event) => {
+    event.target.style.backgroundColor = color;
+  }, false);
+
+  // 4. Return handle to button
+  return button;
+};
+
+function updateScore(current, value, target){
+  current += value;
+  target.innerHTML = "Score: " + current;
+  return current;
+}
