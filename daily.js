7502392b7774c69a7871d9b38444230964a9caef
Russell Gillespie <russellgillespie2018@u.northwestern.edu>
2023-02-09 06:13:13 -0600

    new daily

--- daily.js
+++ daily.js
@@ -1,7 +1,17 @@
+// 2022-02-09
 if (JSON.parse(sessionStorage.getItem('daily')) == null) {
   console.log('Loading Daily');
   sessionStorage.setItem('randDaily', JSON.stringify(
-    [["K"],["X"],["J"],["Y"],["F"],["C"],["Q"],["R"],["M"],["S"],["A"],["U"],["T"],["G"],["Z"],["V"],["W"],["E"],["L"],["H"],["D"],["P"],["I"],["B"],["O"]]
+  [["Q"],["G"],["C"],["K"],["S"],["P"],["V"],["T"],["X"],["Y"],["O"],["I"],["A"],["N"],["B"],["U"],["L"],["W"],["H"],["Z"],["E"],["F"],["M"],["J"[,["R"]]
   ));
-  sessionStorage.setItem('abcDaily', JSON.stringify("N"));
+  sessionStorage.setItem('abcDaily', JSON.stringify("D"));
 };
+
+// 2022-02-08
+// if (JSON.parse(sessionStorage.getItem('daily')) == null) {
+//  console.log('Loading Daily');
+//  sessionStorage.setItem('randDaily', JSON.stringify(
+///    [["K"],["X"],["J"],["Y"],["F"],["C"],["Q"],["R"],["M"],["S"],["A"],["U"],["T"],["G"],["Z"],["V"],["W"],["E"],["L"],["H"],["D"],["P"],["I"],["B"],["O"]]
+//  ));
+//  sessionStorage.setItem('abcDaily', JSON.stringify("N"));
+//};
